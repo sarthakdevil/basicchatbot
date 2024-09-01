0ed4247e3a1770a41bb875d5e94a7374bd1aa413
Sarthakdevil <Sarthakrajesh2005@gmail.com>
2024-09-01 22:56:07 +0530

    pdf resarch bot

---
+++ app.py
@@ -0,0 +1,82 @@
+import os
+import streamlit as st
+from streamlit_chat import message  # streamlit-chat component
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains import ConversationalRetrievalChain
+from langchain_community.vectorstores import FAISS
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langchain.memory import ConversationBufferMemory
+from pypdf import PdfReader
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Chat with PDF")
+    
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "generated" not in st.session_state:
+        st.session_state.generated = []
+    if "past" not in st.session_state:
+        st.session_state.past = []
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = []
+
+    # Reading the PDF
+    text = []
+    reader = PdfReader("jeff.pdf")
+    for page in reader.pages:
+        text.append(page.extract_text())
+    
+    # Joining text
+    full_text = "\n".join(text)
+
+    # Splitting text into chunks
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    chunks = text_splitter.split_text(full_text)
+
+    huggingface_embeddings = HuggingFaceBgeEmbeddings(
+        model_name = "BAAI/bge-small-en-v1.5",
+        model_kwargs = {'device':'cpu'},
+        encode_kwargs={'normalize_embeddings':True}
+    )
+
+    vectorstore = FAISS.from_texts(chunks, huggingface_embeddings)
+
+    memory = ConversationBufferMemory(
+        memory_key="chat_history", 
+        return_messages=True
+    )
+
+    llm = ChatGroq(
+        model="Llama3-8b-8192",
+        temperature=0,
+        max_tokens=None,
+        timeout=None,
+        max_retries=2,
+    )
+
+    chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+
+    # Handle user input
+    user_input = st.text_input("Ask a question about the PDF:")
+    if user_input:
+        response = chain({"question": user_input})
+        st.session_state.past.append(user_input)
+        st.session_state.generated.append(response['answer'])
+        st.session_state.chat_history.append((user_input, response['answer']))
+
+
+    # Display chat history using streamlit-chat
+    if st.session_state.chat_history:
+        for i, (user_msg, bot_msg) in enumerate(st.session_state.chat_history):
+            message(user_msg, is_user=True, key=str(i) + '_user')
+            message(bot_msg, key=str(i))
+
+if __name__ == "__main__":
+    main()
